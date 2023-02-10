f0583e15a2da7c95b2835c2384c2b981d64c4f73
John-Stuart Gonsales <nerveship@gmail.com>
2023-02-10 17:11:15 +0000

    basic income check

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Bills
+{
+    private static decimal min_income = 0;
+
+    public bool ValidIncome(decimal income)
+    {
+        if (income >= min_income)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+class BillCalculator
+{
+    public static void Main()
+    {
+        Bills userBills = new Bills();
+        userBills.ValidIncome(512);
+    }
+}
