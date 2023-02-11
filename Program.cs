fc398f919d49f8b27318c0a0457959e68231172b
John-Stuart Gonsales <nerveship@gmail.com>
2023-02-11 02:27:04 +0000

    added bill accumulation function and some comments

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ public class Bills
     private static decimal Min_Income = 0;
     public decimal Income;
     public int BillNo;
-    public List<float> NoOfBills = new List<float>();
+    public List<float> SumOfBills = new List<float>();
 
+    //Checks if the users income is valid or not
     public bool ValidIncome()
     {
         if (Income > Min_Income)
@@ -15,6 +16,16 @@ public class Bills
             return true;
         }
         return false;
+    }
+
+    //Adds the users number of bills and prompts them for how much each costs
+    public void BillAccum()
+    {
+        for (int i = 0; i < BillNo; i++)
+        {
+            Console.WriteLine($"Bill #{i + 1}: ");
+            SumOfBills.Add(Convert.ToInt32(Console.ReadLine()));
+        }
     }
 
     //Constructor
@@ -28,15 +39,24 @@ class BillCalculator
 {
     public static void Main()
     {
+        //Create new bill object
         Bills userBills = new Bills(0);
+
+        //Welcome message
         Console.WriteLine("Welcome! Let's calculate your bills and savings for this month!");
         Console.WriteLine("Press any key to continue at any time");
         Console.ReadKey();
         Console.Clear();
 
+        //Get users income
         Console.WriteLine("How much income have you made this month?");
-
         userBills.Income = Convert.ToDecimal(Console.ReadLine());
         userBills.ValidIncome();
+        Console.Clear();
+
+        //Get users number of bills
+        Console.WriteLine("How many bills do you have this month?");
+        userBills.BillNo = Convert.ToInt32(Console.ReadLine());
+        userBills.BillAccum();
     }
 }
